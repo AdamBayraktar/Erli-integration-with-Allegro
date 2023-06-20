410ba9dd8c0e20e4a32f3f7d2720c109718b9790
Alemoz EB <adambayraktar@gmail.com>
2023-06-20 13:37:54 +0200

    Add check connection with erli API. Send simply get methods and check if return status code is 200

---
+++ check_connection_erli.py
@@ -0,0 +1,17 @@
+import requests
+import os
+# import your unique api key ERLI
+from config import api_key_erli
+
+
+# request url data
+url = 'https://erli.pl/svc/shop-api/me'
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {api_key_erli}"}
+
+response = requests.get(url, headers=headers)
+if response.status_code == 2001:
+    print("Connection established")
+else:
+    raise Exception("Api key", "Check your api key")
