c4d5a9a52c8fdee6d13abd5a5777cf2e1719dca3
Alemoz EB <adambayraktar@gmail.com>
2023-07-03 16:03:55 +0200

    Add main function where you can check if requests works - get_erli_product_by_id.py

--- get_erli_product_by_id.py
+++ get_erli_product_by_id.py
@@ -2,6 +2,11 @@ import requests
 # import your unique api key ERLI
 from config import api_key_erli
 
+# You can use this main function to check if it works
+def main():
+    example = get_erli_product_by_id("12585141381")
+    print(example)
+
 def get_erli_product_by_id(id):
     url = f'https://erli.pl/svc/shop-api/products/{id}'
     headers = {
@@ -10,3 +15,5 @@ def get_erli_product_by_id(id):
     response = requests.get(url, headers=headers)
     return response.json()
 
+if __name__ == "__main__":
+    main()
