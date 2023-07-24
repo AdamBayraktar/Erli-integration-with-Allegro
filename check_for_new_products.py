c499c337f800c641cb2a69244bda8392bb006e19
Alemoz EB <adambayraktar@gmail.com>
2023-07-24 16:58:09 +0200

    Add function that will display information when everything is up to date - check_for_new_products.py

--- check_for_new_products.py
+++ check_for_new_products.py
@@ -11,22 +11,23 @@ def main():
     for allegro_id in all_allegro_id:
         if allegro_id not in erli_products_id:
             new_products.append(allegro_id)
-    create_txt("new_products.txt", new_products)
+    create_txt("new_products.txt", new_products, msg="There isn't any new products")
     # terminated products
     terminated_products = []
     for erli_id in erli_products_id:
         if erli_id not in all_allegro_id:
             terminated_products.append(erli_id)
-    create_txt("terminated_products.txt", terminated_products)
+    create_txt("terminated_products.txt", terminated_products, msg="There isn't any terminated products")
     
     
-def create_txt(file_name, id_list):
+def create_txt(file_name, id_list, msg="nothing new"):
     with open(file_name, "w") as file:
         if id_list:
             for id in id_list:
                 file.write(id + '\n')
         else:
             file.write("Nothing new")
+            print(msg)
     
 if __name__ == "__main__":
     main()
